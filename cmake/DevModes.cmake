ab980fe2ca3c30414e33a70237239d23d9944965
Anders Rosén <panrosen@gmail.com>
2021-02-21 23:28:54 +0100

    Don't fail the build if doxygen wasn't found

--- cmake/DevModes.cmake
+++ cmake/DevModes.cmake
@@ -1,12 +1,5 @@
 string(TOUPPER ${PROJECT_NAME} UC_PROJECT_NAME)
 
-option(${UC_PROJECT_NAME}_ENABLE_SANITIZERS "Use address, leak and undefined behavior sanitizers" OFF)
-option(${UC_PROJECT_NAME}_ENABLE_COVERAGE "Compile with code coverage analysis" OFF)
-
-if (${UC_PROJECT}_ENABLE_SANITIZERS AND ${UC_PROJECT}_ENABLE_COVERAGE)
-    message(FATAL_ERROR "Coverage and sanitizers can't be enabled at the same time!")
-endif()
-
 set(GNU_CXX_WARNINGS
     -Wall
     -pedantic
@@ -95,24 +88,18 @@ set(${UC_PROJECT_NAME}_DEVMODES_CLANG_CXX_WARNINGS ${DEVMODES_CLANG_CXX_WARNINGS
     CACHE STRING "clang++ warnings to use in developer mode builds"
 )
 
-set(DEV_MODES Dev DevWithASAN DevWithCoverage)
 get_property(IS_MULTI_CONFIG GLOBAL
     PROPERTY GENERATOR_IS_MULTI_CONFIG
 )
-if (IS_MULTI_CONFIG)
-    foreach(MODE ${DEV_MODES})
-        if(NOT MODE IN_LIST CMAKE_CONFIGURATION_TYPES)
-            list(APPEND CMAKE_CONFIGURATION_TYPES "${MODE}")
-        endif()
-    endforeach()
-else()
+if (NOT IS_MULTI_CONFIG)
+    set(DEV_MODES DevWithASAN DevWithCoverage)
     set(ALLOWED_BUILD_TYPES Debug Release RelWithDebInfo MinSizeRel)
     list(APPEND ALLOWED_BUILD_TYPES ${DEV_MODES})
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY
         STRINGS "${ALLOWED_BUILD_TYPES}"
     )
     if(NOT CMAKE_BUILD_TYPE)
-        set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "" FORCE)
+        set(CMAKE_BUILD_TYPE Release CACHE STRING "" FORCE)
     elseif(NOT CMAKE_BUILD_TYPE IN_LIST ALLOWED_BUILD_TYPES)
         message(FATAL_ERROR "Unknown build type: ${CMAKE_BUILD_TYPE}")
     endif()
